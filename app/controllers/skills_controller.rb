53cc96a94a22aed9ed4d6311970959291674109d
Sam Blackman <sblackman@gmail.com>
2016-05-10 14:57:44 -0400

    demonstrate JSON response with client side rendering

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -11,8 +11,11 @@ end
 post '/skills' do
   @skill = Skill.create(name: params[:name])
   if request.xhr? # AJAX is actually what's called an XMLHTTPRequest
-    # just send back the li tag
+    # Server side rendering — responding with HTML
     erb :'/skills/_individual_skill', layout: false, locals: {skill: @skill}
+    # Client side rendering — responding with JSON
+    # (JavaScript Object Notation - JS object as a string)
+    # {name: @skill.name, id: @skill.id}.to_json
   else
     redirect '/skills'
   end
--- public/js/application.js
+++ public/js/application.js
@@ -13,8 +13,28 @@ $(document).ready(function() {
     });
 
     ajaxRequest.done(function(response) {
+      // Server-side rendering version
+      // Expecting HTML back from the server
+      // -----------------------------------
       $("#skills-list").append(response)
-    })
+      
+      // JS cloning version
+      // Expecting JSON back from the server
+      // -----------------------------------
+      // var el = $("#skills-list").find("li").eq(0).clone()
+      // el.find("a").text(skill.name)
+      // el.find("a").attr("href", "/skills/" + skill.id)
+      // $("#skills-list").append(el);
+
+      // JS templating version
+      // Expecting JSON back from the server
+      // -----------------------------------
+      // var skill = JSON.parse(response)
+      // $("#skills-list").append(buildSkillHTML(skill))
+    });
   })
 });
 
+function buildSkillHTML(skill) {
+  return "<li><a href='/skills/" + skill.id + "'>" + skill.name + "</a></li>"
+}
